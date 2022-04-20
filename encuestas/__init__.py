44c1a6d0aafb81e747a64caea494f8f6a55894e0
Matias Caceres Figueroa <MCACERES2017@udec.cl>
2022-04-20 17:49:02 -0400

    master branch "enviar cambios" matias

--- encuestas/__init__.py
+++ encuestas/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# Solo estoy probando la funcion push del VS studio. (matias.)
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
--- run.py
+++ run.py
@@ -4,3 +4,4 @@ from encuestas import app
 #python run.py
 if __name__ == '__main__':
 	app.run(debug=True)
+

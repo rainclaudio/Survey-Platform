c8dc44f401dfda86e4bb6ed3ca9848743fb6d8c4
claudio <rainclaudio25@gmail.com>
2022-04-23 18:54:38 -0400

    commit claudio nada

--- encuestas/models.py
+++ encuestas/models.py
@@ -33,7 +33,7 @@ class Encuesta(db.Model):
     date_posted = db.Column(db.DateTime, default = datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable = False)
     preguntas = db.relationship('Pregunta', backref = 'content', lazy = True)
-
+    
 class Pregunta(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(100), nullable = False)

6a77c130f34c64892182c90062ad6cb358bb0b40
MartinaCadiz <martinacadiz@gmail.com>
2022-04-24 18:31:36 -0400

    auten usuario - marti

--- encuestas/models.py
+++ encuestas/models.py
@@ -1,8 +1,12 @@
 from datetime import datetime
-from encuestas import db
+from encuestas import db, login_manager_
+from flask_login import UserMixin
 
+@login_manager_.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
--- encuestas/routes.py
+++ encuestas/routes.py
@@ -1,10 +1,10 @@
 from turtle import title
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from datetime import datetime
-from encuestas import app,db
+from encuestas import app,db, bcrypt
 from encuestas.forms import CrearEncuestaForm, CrearItemForm, CrearPreguntaForm, RegistrationForm, LoginForm
 from encuestas.models import Encuesta, Item, User, Post, Pregunta
-
+from flask_login import login_user, current_user,logout_user, login_required
 
 posts = [
     {
@@ -127,22 +127,32 @@ def add_item(encuesta_id,pregunta_id):
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        hash_pass = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username =form.username.data, email=form.email.data, password =  hash_pass)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Creaste tu cuenta, bienvenid@ {form.username.data}!', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email = form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+            login_user(user,remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('No puedes entrar, vuelve a checkear la contraseña o usuario.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
@@ -169,3 +179,14 @@ def publicar_encuesta(encuesta_id,total_pregs,bool_items):
         db.session.commit()
         flash('Your post #' + str(encuesta_id) + ' has been posted!', 'success')
         return redirect(url_for('home'))
+
+@app.route("/profile")
+@login_required
+def profile():
+    return render_template('profile.html', title='Profile')
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
